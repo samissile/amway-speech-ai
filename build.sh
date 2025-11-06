8f190b0568db6996bc157f761ef228f67222b099
samissile <fbtpsilam@gmail.com>
2025-11-06 23:41:26 +0800

    Delete build.sh
    delete build.sh change to Docker

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Install system dependencies
-apt-get update
-apt-get install -y ffmpeg libavcodec-extra
-
-# Install Python dependencies
-pip install -r requirements.txt
